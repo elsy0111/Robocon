9c686c0045da248a84c3a2bd53747b85836cadef
elsy0111 <kpp0193@gmail.com>
2022-05-26 12:31:29 +0900

    prefix

---
+++ 授業スライド/制御授業資料1/Code/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("hel, world\n");
+    return 0;
+}
---
+++ 授業スライド/制御授業資料1/Code/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "hello, world-tinkoikuikusikosiko" << std::endl;
+    return 0;
+}
