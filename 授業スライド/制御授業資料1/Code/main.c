9282246cabbbb2c3c8d6a75278bd83fdbc7d03b9
elsy0111 <kpp0193@gmail.com>
2022-05-26 12:46:05 +0900

    add zip file

--- 授業スライド/制御授業資料1/Code/main.c
+++ 授業スライド/制御授業資料1/Code/main.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
 
-int main(void)
+int main()
 {
-    printf("hel, world\n");
+    char *filename = "text.txt";
+    FILE *fp = fopen(filename, "r");
+
+    if (fp == NULL)
+    {
+        printf("Error: could not open file %s", filename);
+        return 1;
+    } else {
+		printf("%s file opened!\n", filename);
+	}
+
+    // close the file
+    fclose(fp);
+
     return 0;
 }
