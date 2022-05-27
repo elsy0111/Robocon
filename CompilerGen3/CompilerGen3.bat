a6f529cd12c65044af596f4e0f66307e72b1ead1
yorugo0 <yorugo0@gmail.com>
2022-05-27 16:54:51 +0900

    Compiler3世

---
+++ CompilerGen3/CompilerGen3.bat
@@ -0,0 +1,65 @@
+﻿@REM　初期化など &
+echo　著作 : 片野凱介 &
+echo off
+chcp 65001
+
+@REM グローバル変数 &
+set LangTypeC=
+set LangTypeCpp=
+set InputFilePath=
+set RunningPath=
+set GccPath=
+
+@REM Pathの定義 &
+set /p InputFilePath="ソースコードの絶対ファイルパスを入力 : "
+set RunningPath=%~dp0
+set GccPath=C:\Xilinx\14.7\ISE_DS\ISE\gnu\MinGW\Beta\nt64\bin
+
+@REM 前処理 &
+echo 上書き出力なので、実行ファイルを事前に移動させておいてください。
+set LangTypeC=%InputFilePath:~-2%
+set LangTypeCpp=%InputFilePath:~-4%
+
+@REM メイン処理 &
+if %LangTypeC% == .c (
+    echo 選択された言語：c
+
+    cd /d %GccPath%
+    echo コンパイラの出力結果 : 
+    echo - * - * - * - * - * - * - * - * - * - * -
+    gcc -Wall %InputFilePath% -o %RunningPath%OutC
+    echo - * - * - * - * - * - * - * - * - * - * -
+
+    echo;
+    echo 実行ファイルの実行 : 
+    echo - * - * - * - * - * - * - * - * - * - * -
+    %RunningPath%OutC
+    echo - * - * - * - * - * - * - * - * - * - * -
+    echo;
+
+    echo %RunningPath%OutC.exe に上書き出力されています。
+    pause
+
+) else if %LangTypeCpp% == .cpp (
+    echo 選択された言語：cpp
+
+    cd /d %GccPath%
+    echo コンパイラの出力結果 : 
+    echo - * - * - * - * - * - * - * - * - * - * -
+    g++ -Wall -o %RunningPath%OutCpp %InputFilePath%
+    echo - * - * - * - * - * - * - * - * - * - * -
+
+    echo;
+    echo 実行ファイルの実行 : 
+    echo - * - * - * - * - * - * - * - * - * - * -
+    %RunningPath%OutCpp
+    echo - * - * - * - * - * - * - * - * - * - * -
+    echo;
+
+    echo %RunningPath%OutCpp.exe に上書き出力されています。
+    pause
+
+) else (
+    echo cまたはcpp以外が入力されました。プログラムを終了します。
+    pause
+)
---
+++ CompilerGen3/RunningBatch.bat
@@ -0,0 +1,26 @@
+﻿@REM　初期化など &
+echo　著作 : 片野凱介 &
+echo off
+chcp 65001
+
+@REM グローバル変数 &
+set InputFilePath=
+set GccPath=
+
+@REM Pathの定義 &
+set /p InputFilePath="実行ファイルの絶対ファイルパスを入力 : "
+set GccPath=C:\Xilinx\14.7\ISE_DS\ISE\gnu\MinGW\Beta\nt64\bin
+
+@REM 前処理 &
+echo CompilerGen3.batでコンパイルした実行ファイルのみ出力されます。
+
+@REM メイン処理 &
+    cd /d %GccPath%
+
+    echo;
+    echo 実行ファイルの実行 : 
+    echo - * - * - * - * - * -
+    %InputFilePath%
+    echo - * - * - * - * - * -
+    echo;
+    pause
